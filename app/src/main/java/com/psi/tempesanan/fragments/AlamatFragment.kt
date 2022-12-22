4bed61883f1e13a35da2f945b2ec451d8a003f72
tulistyanairfany <tulistyanairfany23@gmail.com>
2022-12-22 14:12:42 +0700

    unicorn

--- app/src/main/java/com/psi/tempesanan/fragments/AlamatFragment.kt
+++ app/src/main/java/com/psi/tempesanan/fragments/AlamatFragment.kt
@@ -1,15 +1,17 @@
 package com.psi.tempesanan.fragments
 
+import android.R
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.psi.tempesanan.R
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_alamat.*
+
 
 
 class AlamatFragment : Fragment() {
@@ -20,17 +22,19 @@ class AlamatFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
         ): View? {
-            return inflater.inflate(R.layout.fragment_alamat, container, false)
+            val v: View =
+                inflater.inflate(com.psi.tempesanan.R.layout.fragment_alamat, container, false)
 
-            btnalamat.setOnClickListener {
-                val openURL = Intent(android.content.Intent.ACTION_VIEW)
-                openURL.data = Uri.parse("https://goo.gl/maps/J42mZsKTWDWuYSKQ8")
-                startActivity(openURL)
+
+            val listnrr: View.OnClickListener = object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    val Alamat = Intent(android.content.Intent.ACTION_VIEW)
+                    Alamat.data = Uri.parse("https://goo.gl/maps/J42mZsKTWDWuYSKQ8")
+                    startActivity(Alamat)
+                }
             }
-
+            val btnAlamat = v.findViewById<View>(com.psi.tempesanan.R.id.btn_alamat) as Button
+            btnAlamat.setOnClickListener(listnrr)
+            return v
         }
-
 }
-
-
-
--- app/src/main/java/com/psi/tempesanan/fragments/EditTransaksiActivity.kt
+++ app/src/main/java/com/psi/tempesanan/fragments/EditTransaksiActivity.kt
@@ -1,16 +1,32 @@
 package com.psi.tempesanan.fragments
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.psi.tempesanan.R
+import kotlinx.android.synthetic.main.activity_edit_transaksi.*
+import java.util.*
 
 class EditTransaksiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_transaksi)
+
+
+    //Kalender
+    val c = Calendar.getInstance()
+    val year = c.get(Calendar.YEAR)
+    val month = c.get(Calendar.MONTH)
+    val day = c.get(Calendar.DAY_OF_MONTH)
+
+    //Button Klik Kalender
+    pilih_tanggal.setOnClickListener{
+        val dpd = DatePickerDialog(this,  DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+            //date
+            edit_tgltransaksi.setText("" + mDay + "/" + mMonth + "/" + mYear)
+        }, year,month,day)
+            dpd.show()
+        }
     }
-
-    fun getValues(view: View) {}
-
-}
+    }
